08a6b6d15c662fb3525aed3cb0515bbf4981ab83
svarion <alessandro.scisci@edu.itspiemonte.it>
2022-11-23 12:00:18 +0100

    added pokeball file with higher resolution

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PokeStack } from './src/navigation/PokeStack';
-import { BottomGenStack } from './src/navigation/BottomGenStack';
 import AnimatedSplash from 'react-native-animated-splash-screen';
 
 const AppNavigation = () => {
@@ -14,13 +13,11 @@ const AppNavigation = () => {
         }, 1200);
     }, []);
 
-
-
     return (
         <AnimatedSplash
             translucent={true}
             isLoaded={isLoaded}
-            logoImage={require("./src/assets/adaptive-icon.png")}
+            logoImage={require("./src/assets/images/pokeball.png")}
             backgroundColor={"#ffffff"}
             logoHeight={150}
             logoWidth={150}>
