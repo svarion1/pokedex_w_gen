452e9fbbcbd3edc8d02c95b156d096f156252c71
svarion <alessandro.scisci@edu.itspiemonte.it>
2022-11-23 11:47:52 +0100

    removed icon folder and added animated splash screen

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { PokeStack } from './src/navigation/PokeStack';
 import { BottomGenStack } from './src/navigation/BottomGenStack';
+import AnimatedSplash from 'react-native-animated-splash-screen';
 
 const AppNavigation = () => {
+
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
+    const test = React.useEffect(() => {
+        setTimeout(() => {
+            setIsLoaded(true);
+        }, 1200);
+    }, []);
+
+
+
     return (
-        <NavigationContainer>
-            <PokeStack/>
-        </NavigationContainer>
+        <AnimatedSplash
+            translucent={true}
+            isLoaded={isLoaded}
+            logoImage={require("./src/assets/adaptive-icon.png")}
+            backgroundColor={"#ffffff"}
+            logoHeight={150}
+            logoWidth={150}>
+            <NavigationContainer screenProps={{ setAppLoaded: test }}>
+                <PokeStack />
+            </NavigationContainer>
+        </AnimatedSplash>
     );
 };
 
